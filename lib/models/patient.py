595395b4e104e7d2e3961c9e9a7dfcc56ae5a11a
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-28 00:37:13 +0300

    _init_.patient

--- lib/models/patient.py
+++ lib/models/patient.py
@@ -1,2 +1,9 @@
 class Patient:
     all = []
+
+    def __init__(self, name, age, gender):
+        self.id = len(Patient.all)+1
+        self.name = name
+        self.age = age
+        self.gender = gender
+        Patient.all.append(self)
