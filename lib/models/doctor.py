fc85f00c02fb14702bd9d59e8bb6ed344c29e855
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-28 00:40:31 +0300

    _init_.Doctor

--- lib/models/doctor.py
+++ lib/models/doctor.py
@@ -1,2 +1,8 @@
 class Doctor:
     all = []
+
+    def __init__(self, name, specialization):
+        self.id = len(Doctor.all) + 1
+        self.name = name
+        self.specializtion = specialization
+        Doctor.all.append(self)
