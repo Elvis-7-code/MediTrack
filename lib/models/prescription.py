ae4e428d4a4f76968f83bda5a2b92007f280577d
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-28 00:51:33 +0300

    _init_.prescription

--- lib/models/prescription.py
+++ lib/models/prescription.py
@@ -1,2 +1,11 @@
 class Prescription:
     all = []
+
+    def _init_(self, patient, doctor, medication, dosage, instructions):
+        self.id = len(Prescription.all) +1
+        self.patient = patient
+        self.doctor = doctor
+        self.medication = medication
+        self.dosage = dosage
+        self.instruction = instructions 
+        Prescription.all.append(self)
