2050bd961440221635aac7dddfd9634cf97e9232
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-28 00:45:42 +0300

    _init_.appointment

--- lib/models/appointment.py
+++ lib/models/appointment.py
@@ -1,2 +1,10 @@
 class Appointment:
     all = []
+
+    def __init__(self, patient, doctor, appointment_date, notes=""):
+        self.id = len(Appointment.all)+1
+        self.patient = patient
+        self.doctor = doctor
+        self.appointment_date = appointment_date
+        self.notes = notes
+        Appointment.all.append(self)
