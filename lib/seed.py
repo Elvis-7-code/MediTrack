f84690c90c2991edc1d443659111e3a439eb59f3
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-28 16:22:13 +0300

    seed_data(d1, d2)

--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,6 @@
 def seed_data():
     p1 = Patient("Alice", 30, "Female")
     p2 = Patient("Bob", 45, "Male")
+
+    d1 = Doctor("Dr. Smith", "Cardiology")
+    d2 = Doctor("Dr. Abby", "Dermatology")
