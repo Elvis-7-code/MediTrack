80ac5452835be3db2424b1cfe8a94105711960ef
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-28 16:39:21 +0300

    def_main.cli

--- lib/cli.py
+++ lib/cli.py
@@ -2,3 +2,8 @@ from lib.models.patient import Patient
 from lib.models.doctor import Doctor
 from lib.models.appointment import Appointment
 from lib.models.prescription import Prescription
+
+def main():
+    print("=== MediTrack Hospital Management System ===\n")
+
+    
