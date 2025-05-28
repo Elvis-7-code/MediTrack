98fe32a203eb7407560dfcf2a6bd9ca7c9558ab3
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-28 16:47:26 +0300

    final (debug.py)changes

--- lib/debug.py
+++ lib/debug.py
@@ -2,3 +2,4 @@ from lib.models.patient import Patient
 from lib.models.doctor import Doctor
 from lib.models.appointment import Appointment
 from lib.models.prescription import Prescription
+import ipdb; ipdb.set_trace()
