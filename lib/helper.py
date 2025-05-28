522c22edbdecf9c4bd24f75448da8ccbe518c512
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-28 16:55:39 +0300

    find_doctor_by_id

--- lib/helper.py
+++ lib/helper.py
@@ -3,3 +3,5 @@ from lib.models.patient import Doctor
 
 def find_patient_by_id(patient_id):
     return((p for p in Patient.all if p.id == patient_id),)
+def find_doctor_by_id(doctor_id):
+    return next((d for d in Doctor.all if d.id == doctor_id), None)
